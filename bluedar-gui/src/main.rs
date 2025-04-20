7996413f2c5b1ae55a743835cd286991b94c7981
div72 <noreply@div72.xyz>
2025-04-20 18:49:26 +0300

    gui: mock device render

--- bluedar-gui/src/main.rs
+++ bluedar-gui/src/main.rs
@@ -5,18 +5,35 @@ pub fn main() -> iced::Result {
     iced::run("Bluedar GUI", Application::update, Application::view)
 }
 
+#[derive(Debug, Clone)]
+struct Device {
+    mac_address: [u8; 6],
+    display_name: Option<String>,
+    coords: (f32, f32),
+}
+
 #[derive(Debug, Clone, Copy)]
 enum Message {
 }
 
 #[derive(Debug)]
 struct Radar {
+    devices: Vec<Device>,
 }
 
 const GRID_LINE_THICKNESS: f32 = 1.0;
 const GRID_LINE_SPACING: usize = 20;
+const DEVICE_CIRCLE_RADIUS: f32 = 20.0;
 
 impl Radar {
+    fn device_color(device: &Device) -> Color {
+        Color::from_rgb8(
+            device.mac_address[0] ^ device.mac_address[3],
+            device.mac_address[1] ^ device.mac_address[4],
+            device.mac_address[2] ^ device.mac_address[5]
+        )
+    }
+
     fn draw_bg_grid(&self, frame: &mut canvas::Frame, bounds: &Rectangle) {
         let bg = canvas::Path::rectangle(Point::new(0.0, 0.0), bounds.size());
 
@@ -32,6 +49,19 @@ impl Radar {
             let line = canvas::Path::rectangle(Point::new(0.0, y as f32 - GRID_LINE_THICKNESS / 2.0), Size::new(bounds.width, GRID_LINE_THICKNESS));
 
             frame.fill(&line, Color::from_rgb(0.3, 0.3, 0.3));
+        }
+    }
+
+    fn draw_devices(&self, frame: &mut canvas::Frame, _bounds: &Rectangle) {
+        let devices = [
+            Device { mac_address: [0x22, 0xb4, 0xcd, 0x50, 0xa7, 0xda], display_name: None, coords: (500.0, 500.0)},
+            Device { mac_address: [0x51, 0xd2, 0xce, 0x49, 0x50, 0x93], display_name: None, coords: (300.0, 700.0)}
+        ];
+
+        for device in devices {
+            let device_circle = canvas::Path::circle(Point::new(device.coords.0, device.coords.1), DEVICE_CIRCLE_RADIUS);
+
+            frame.fill(&device_circle, Radar::device_color(&device));
         }
     }
 }
@@ -50,6 +80,7 @@ impl<Message> canvas::Program<Message> for Radar {
         let mut frame = canvas::Frame::new(renderer, bounds.size());
 
         self.draw_bg_grid(&mut frame, &bounds);
+        self.draw_devices(&mut frame, &bounds);
 
         vec![frame.into_geometry()]
     }
@@ -57,6 +88,7 @@ impl<Message> canvas::Program<Message> for Radar {
 
 #[derive(Default)]
 struct Application {
+    devices: Vec<Device>,
 }
 
 impl Application {
@@ -65,7 +97,7 @@ impl Application {
 
     fn view(&self) -> Column<Message> {
         column![
-            canvas(Radar { })
+            canvas(Radar { devices: self.devices.clone() })
             .width(Fill)
             .height(Fill)
         ]
